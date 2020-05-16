0003302133e306a88a700b10e743bc359b271f14
sonia <sonia@DESKTOP-BGUL1JV>
2020-05-16 10:59:15 +0200

    commit

--- src/main/java/it/polito/tdp/quadrato/model/RisolviQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/RisolviQuadrato.java
@@ -7,6 +7,7 @@ public class RisolviQuadrato {
 	private int N ; // lato del quadrato
 	private int N2 ; // numero di caselle (N^2)
 	private int magica ; // costante magica
+	private List<List<Integer>> soluzioni;
 	
 	public RisolviQuadrato(int N){
 		this.N = N;
@@ -14,23 +15,33 @@ public class RisolviQuadrato {
 		this.magica = N*(N2+1)/2;
 	}
 	// Calcola tutti i quadrati magici
-	public void quadrati() {
+	public List<List<Integer>> quadrati() {
 		
 		List<Integer> parziale = new ArrayList<Integer>();
 		int livello = 0;
+		this.soluzioni = new ArrayList<List<Integer>>();
 		ricorsiva(parziale, livello);
+		return this.soluzioni;
 	}
-	// Procedura ricorsiva ver e propria
+	// Procedura ricorsiva vera e propria
 	private void ricorsiva(List<Integer> parziale, int livello) {
 		
 		if(livello == this.N2) {	// Caso terminale
 			
 			if(controlla(parziale)) {	// Il quadrato è magico
-				System.out.println(parziale);
+				//System.out.println(parziale);
+				this.soluzioni.add(new ArrayList<Integer>(parziale));		// Quando porto fuori dei dati devo portare fuori il valore, non il suo riferimento
 			}
 			return;
 		}
+		// Devo aggiungere controlli intemedi quando il livello è multiplo di N, ovvero ho finito una riga
 		
+		if(livello%N == 0 && livello != 0) {		// La funzione controlla se livello è un multiplo di N
+			if(!controllaRiga(parziale, livello/N-1)) {
+				return;	// Potatura dell'albero di ricerca
+			}
+		}
+		// Cado intermedio
 		for(int valore=1; valore <= this.N2; valore++) {
 			if(!parziale.contains(valore)) {		// Se non lo contiene allora posso provarlo
 				parziale.add(valore);
@@ -87,5 +98,14 @@ public class RisolviQuadrato {
 			return false ;
 
 		return true ;
+	}
+	
+	private boolean controllaRiga(List<Integer> parziale, int riga) {
+		int somma = 0;
+		
+		for(int col=0; col<N; col++) {
+			somma+= parziale.get(riga*N+col);
+		}
+		return (somma==magica);
 	}
 }
--- src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
@@ -5,8 +5,13 @@ import java.util.List;
 public class TestRisolviQuadrato {
 
 	public static void main(String args[]) {
-		RisolviQuadrato r = new RisolviQuadrato(3) ;
-		r.quadrati();
+		RisolviQuadrato r = new RisolviQuadrato(4) ;
+		
+		List<List<Integer>> soluzioni =r.quadrati();
+		
+		for(List<Integer> sol : soluzioni) {
+			System.out.println(sol);
+		}
 		
 	}
 	
